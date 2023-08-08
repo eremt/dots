d10ae64e0f5c29c3ffe085fda2ff99f42f358e4b
Troy <eremtroy@gmail.com>
2023-08-09 01:06:22 +0200

    neovim: setup completions

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,19 +1,82 @@
--- LSP Config
+-- LSP and competion config
 --
--- Plugins:
--- - nvim-lspconfig: https://github.com/neovim/nvim-lspconfig
+-- Required plugins:
+-- - nvim-lspconfig:     https://github.com/neovim/nvim-lspconfig
+-- - nvim-cmp:           https://github.com/hrsh7th/nvim-cmp/
+-- - ultisnips:          https://github.com/SirVer/ultisnips/
+-- - cmp-nvim-lsp:       https://github.com/hrsh7th/cmp-nvim-lsp/
+-- - cmp-nvim-ultisnips: https://github.com/quangnguyen30192/cmp-nvim-ultisnips
+-- - cmp-path:           https://github.com/hrsh7th/cmp-path
+-- - cmp-buffer:         https://github.com/hrsh7th/cmp-buffer
+-- - cmp-cmdline:        https://github.com/hrsh7th/cmp-cmdline
 --
--- Servers:
--- - Bash:        npm install -g bash-language-server
--- - TypeScript:  npm install -g typescript typescript-language-server
--- - Python:      npm install -g pyright
--- - clangd:      https://clangd.llvm.org/installation.html
--- - SQL:         npm i -g sql-language-server
--- - Rust:        https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
--- - Go:          go install golang.org/x/tools/gopls@latest
+-- Language servers:
+-- - Bash:       npm install -g bash-language-server
+-- - TypeScript: npm install -g typescript typescript-language-server
+-- - Python:     npm install -g pyright
+-- - clangd:     https://clangd.llvm.org/installation.html
+-- - SQL:        npm i -g sql-language-server
+-- - Rust:       https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
+-- - Go:         go install golang.org/x/tools/gopls@latest
 
 local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 local servers = { 'bashls', 'tsserver', 'pyright', 'clangd', 'sqlls', 'rust_analyzer', 'gopls' }
+
+local cmp = require('cmp')
+cmp.setup {
+  snippet = {
+    expand = function (args)
+      vim.fn["UltiSnips#Anon"](args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'ultisnips' },
+  }, {
+    { name = 'buffer' },
+  },
+}
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
 
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
@@ -48,6 +111,7 @@ end
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach
+    capabilities = capabilities,
+    on_attach = on_attach,
   }
 end
