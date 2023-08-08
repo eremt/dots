d1c9eb7e53633db89819325085e5126ad23fde5d
Troy <eremtroy@gmail.com>
2023-08-08 21:55:15 +0200

    neovim: config, lsp and aliases

---
+++ .config/nvim/init.vim
@@ -0,0 +1,66 @@
+colorscheme vinimal
+
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+set noswapfile
+
+let mapleader=' '
+
+nnoremap ; :
+nnoremap Q @@
+
+" completions
+set complete=.,w,b,u,t,i
+set completeopt=menu,noinsert
+inoremap <A-Space> <C-x><C-o>
+
+" never enough methods to quit
+nnoremap <leader>x :x<CR>
+nnoremap <leader>X :cq<CR>
+
+" disable yank when pasting in visual mode
+vnoremap p "_dP
+
+" window commands
+" no ctrl
+nnoremap <leader>w <C-w>
+" resize vertical
+nnoremap <leader>- :exe "vertical resize -20"<CR>
+nnoremap <leader>= :exe "vertical resize +20"<CR>
+
+" search/replace
+" word under cursor
+nnoremap <leader>/ yiw:%s/<C-r>"/<C-r>"
+" visual selection
+vnoremap <leader>/ y:%s/<C-r>"/<C-r>"
+
+" compile
+" with Makefile
+nnoremap <leader>m :!make<CR>
+" without Makefile [file].c => [file]
+nnoremap <leader>M :!gcc -g -o %:r %<CR>
+
+" debugger
+packadd termdebug
+let g:termdebug_wide=1
+nnoremap <leader>dd :Termdebug<CR>
+nnoremap <leader>df :Termdebug ./%:r<CR>
+nnoremap <leader>dr :Run<CR>
+nnoremap <leader>b :Break<CR>
+nnoremap <leader>B :Clear<CR>
+nnoremap <leader>s :Step<CR>
+nnoremap <leader>n :Over<CR>
+nnoremap <leader>N :Continue<CR>
+nnoremap <leader>v :Evaluate<CR>
+nnoremap <leader>dx :call TermDebugSendCommand("exit")<CR>
+
+" fzf.vi
+" Debian
+source /usr/share/doc/fzf/examples/fzf.vim
+" Arch
+" source /usr/share/vim/vimfiles/plugin/fzf.vim
+nnoremap <C-p> :Files<CR>
+
+lua require('config/lsp')
---
+++ .config/nvim/lua/config/lsp.lua
@@ -0,0 +1,53 @@
+-- LSP Config
+--
+-- Plugins:
+-- - nvim-lspconfig: https://github.com/neovim/nvim-lspconfig
+--
+-- Servers:
+-- - Bash:        npm install -g bash-language-server
+-- - TypeScript:  npm install -g typescript typescript-language-server
+-- - Python:      npm install -g pyright
+-- - clangd:      https://clangd.llvm.org/installation.html
+-- - SQL:         npm i -g sql-language-server
+-- - Rust:        https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
+-- - Go:          go install golang.org/x/tools/gopls@latest
+
+local lspconfig = require('lspconfig')
+local servers = { 'bashls', 'tsserver', 'pyright', 'clangd', 'sqlls', 'rust_analyzer', 'gopls' }
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- jumping
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+
+  -- documentation
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
+  -- formatting
+  vim.keymap.set('n', '<Tab>', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach
+  }
+end
